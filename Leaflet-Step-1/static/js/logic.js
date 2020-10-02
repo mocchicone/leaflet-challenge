8d7464ccbab73d8e6094eaa7f8092adf028c8459
Mike Occhicone <65381495+mocchicone@users.noreply.github.com>
2020-10-02 11:27:52 -0700

    added color and size to circle markers based on eq depth and magnitude

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,10 +1,12 @@
-// // Store our API endpoint inside queryUrl
+// Url to the Json data = US Earthquakes from the past week
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
+// Create Map Object
 var myMap = L.map("map", {
-  center: [37.09, -95.71],
-  zoom: 5})
+  center: [38, -112],
+  zoom: 6})
 
+// Create streetmap layer  
 var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
@@ -13,43 +15,42 @@ var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
   id: "mapbox/streets-v11",
   accessToken: API_KEY}).addTo(myMap)
 
-  function chooseColor(x) {
-    switch (x) {
-    case (x < 10):
-      return "yellow";
-    case (x < 30):
-      return "green";
-    case (x < 60):
-      return "orange";
-    case (x < 70):
-      return "blue";
-    case (x < 100):
-      return "purple";
-    default:
-      return "red";
-    }
-  }
 
+// Pull data from json.
 d3.json(queryUrl, function(response) {
     console.log(response);
     eq = response.features
-    for (var i = 0; i < eq.length; i++) {
-      var location = eq[i].geometry;
-      var eqprop = eq[i].properties;
+    for (var x = 0; x < eq.length; x++) {
+      var location = eq[x].geometry;
+      var eqprop = eq[x].properties;
+      var magnitude = eq[x].properties.mag
+      var depth = location.coordinates[2]
 
-      
+   //  function to set color based on depth   
+      function setColor(d) {
+        if (d<10){return "yellow";}
+        else if (d<20){return "greenyellow";}
+        else if (d<30){return "green";}
+        else if (d<40){return "orange";}
+        else {return "red";}
+      }
+     
+     // created circle markers based on magnitude and depth, and attached popups 
       L.circle([location.coordinates[1], location.coordinates[0]], {
-        color: chooseColor(eqprop.mag),
-        fillColor: chooseColor(eqprop.mag),
+        color: "black",
+        weight: .3,
+        fillColor: setColor(depth),
         fillOpacity: .75,
-        radius: 10000
+        radius: (magnitude * 5000)
+      
       }).bindPopup("<h2>" + eqprop.place +
       "</h2><hr><p>" + new Date(eqprop.time) + 
-      "<br>Magnitude: " + eqprop.mag + 
-      "<br>Depth: " + location.coordinates[2] + "</p>")
+      "<br>Magnitude: " + magnitude + 
+      "<br>Depth: " + depth + "</p>")
       .addTo(myMap);
       }})
  
+
 
 
 
